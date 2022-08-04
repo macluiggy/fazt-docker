b731f070576f9d855e3d7b7134db8c52f1e487b9
Luiggy Macias <74107886+macluiggy@users.noreply.github.com>
2022-08-04 12:44:01 +0000

    gfgg

--- common-commands.ts
+++ common-commands.ts
@@ -8,3 +8,10 @@
 // docker exec -it container_id ---- ejecuta una session interactiva en el contenedor
 // docker stop container_id ---- para detener el contenedor
 // docker run -d container_id ---- para ejecutar un contenedor en background
+// docker run -p 3000:80 nginx ---- esto quiere decir que internamente el contenedor escucha en el puerto 80 y lo redirige a 3000 localmente
+// docker ps -a ---- muestra todos los contenedores que han sido ejecutados
+// docker ps -aq ---- muestra todos los contenedores
+// docker rm container_id ---- para eliminar un contenedor
+// docker rm $(docker ps -aq) ---- para eliminar todos los contenedores
+// docker stop $(docker ps -aq) ---- para detener todos los contenedores
+// docker run -p 80:80 -p 3000:80 ... -d nginx ---- de esta manera podemos ejecutar el mismo contenedor en varios puertos
