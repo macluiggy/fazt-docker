5746743d37782a3552b3e53fca04db10c8ba96ca
Luiggy Macias <74107886+macluiggy@users.noreply.github.com>
2022-08-04 13:19:48 +0000

    FDFDF

--- common-commands.ts
+++ common-commands.ts
@@ -13,5 +13,9 @@
 // docker ps -aq ---- muestra todos los contenedores
 // docker rm container_id ---- para eliminar un contenedor
 // docker rm $(docker ps -aq) ---- para eliminar todos los contenedores
+// docker rm $(docker ps -aq) -f ---- fuerza la eliminacion de los contenedores incluso si se estan ejecutando
 // docker stop $(docker ps -aq) ---- para detener todos los contenedores
 // docker run -p 80:80 -p 3000:80 ... -d nginx ---- de esta manera podemos ejecutar el mismo contenedor en varios puertos
+// docker run -d -p 3306:3306 --name mydatabase mysql ---- para ejecutar un contenedor con un nombre y un puerto y un alias
+// docker run -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=my-secret-pw --name mydatabase mysql ---- para ejecutar un contenedor con un nombre y un puerto y un alias y una variable de entorno}
+// docker ps --format "table {{.ID}}\t{{.Names}}\t{{.Image}}\t{{.Status}}" ---- muestra todos los contenedores en una tabla de una forma mas bonita
