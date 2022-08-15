ada7ca34c45f539b2197cf38a19bc95ae99c4e51
luiggy macias <ferrinluiggy@gmail.com>
2022-08-15 09:08:25 -0500

    cdd

--- server-hola-mundo/index.js
+++ server-hola-mundo/index.js
@@ -1,3 +1,4 @@
+//@ts-check
 import express from "express";
 import mongoose from "mongoose";
 
@@ -8,3 +9,35 @@ const Animal = mongoose.model(
     estado: String,
   })
 );
+
+const app = express();
+const router = express.Router();
+
+mongoose.connect(
+  "mongodb://luiggy:password@localhost:27017/miapp?authSource=admin"
+);
+
+app.get("/", async (req, res) => {
+  console.log("listando...");
+  const animales = await Animal.find();
+  return res.send(animales);
+});
+
+app.get("/crear", async (req, res) => {
+  console.log("creando...");
+  const animal = new Animal({
+    tipo: "Chanchito",
+    estado: "Feliz",
+  });
+  await animal.save();
+  return res.send(animal);
+});
+
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Servidor corriendo en el puerto", port);
+});
+
+// export default app
+// para conectarse a un contenedor docker de mongo
+// docker create -p27017:27017 --name monguito -e MONGO_INITDB_ROOT_USERNAME=luiggy -e MONGO_INITDB_ROOT_PASSWORD=password -e MONGO_INITDB_DATABASE=animales -e MONGO_INITDB_ROOT_USERNAME=admin -e MONGO_INITDB_ROOT_PASSWORD=admin -e MONGO_INITDB_DATABASE=animales -d mongo
