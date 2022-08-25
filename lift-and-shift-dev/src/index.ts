7809aba0a5e865b0f269974c29752bd2750d1740
luiggy macias <ferrinluiggy@gmail.com>
2022-08-25 06:35:35 -0500

    fffff

--- lift-and-shift-dev/src/index.ts
+++ lift-and-shift-dev/src/index.ts
@@ -1,14 +1,17 @@
+//@ts-check
 import express, { Request, Response } from "express";
+import db from "./db";
 import dotenv from "dotenv";
 dotenv.config();
 
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+app.get("/", (_req: Request, _res: Response) => {
+  _res.send("Hello World!");
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
+  db.runMigrations();
 });
